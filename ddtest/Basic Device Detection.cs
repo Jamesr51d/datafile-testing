50b62e3cc357d92a3a3a9405e41f926b12867c68
James Rogers <JamesR@51degrees.com>
2023-09-13 15:55:07 +0100

    If you're reading this I'm sorry for my crappy code

---
+++ ddtest/Basic Device Detection.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+//using ConsoleApp;
+using FiftyOne.DeviceDetection;
+using FiftyOne.DeviceDetection.Cloud.FlowElements;
+using FiftyOne.Pipeline.Core.Data;
+using FiftyOne.Pipeline.Engines.Data;
+
+
+            string newFile = @"C:\Users\jamesr\Downloads\NEW 08052023TAC-HashV41.hash";
+            string CurrentFile = @"C:\Users\jamesr\Downloads\08052023TAC-HashV41.hash";
+            string aprilFile = @"C:\Users\jamesr\Downloads\10-04 TAC-HashV41.hash";
+
+            var pipeline = new DeviceDetectionPipelineBuilder()
+             .UseOnPremise(CurrentFile, false)
+             .SetAutoUpdate(false)
+             .SetDataFileSystemWatcher(false)
+             .Build();
+
+
+            string userAgent = $"Mozilla/5.0 (Linux; Android 11; A509DL Build/RP1A.200720.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/113.0.5672.77 Mobile Safari/537.36";
+
+using (var data = pipeline.CreateFlowData())
+{
+   
+    data.AddEvidence("header.user-agent", userAgent);
+    data.Process();
+    var output = data.Get<IDeviceData>();
+
+    // string row = $"{output.HardwareVendor.GetHumanReadable()},{output.HardwareModel.GetHumanReadable()},{output.HardwareName.GetHumanReadable()},{output.PlatformVendor.GetHumanReadable()},{output.PlatformName.GetHumanReadable()},{output.PlatformVersion.GetHumanReadable()},{output.BrowserVendor.GetHumanReadable()},{output.BrowserName.GetHumanReadable()},{output.BrowserVersion.GetHumanReadable()},{output.HardwareFamily.GetHumanReadable()},{output.OEM.GetHumanReadable()}";
+    Console.WriteLine("UserAgent        :       " + userAgent);
+    Console.WriteLine("HardwareVendor:      " + output.HardwareVendor.GetHumanReadable());
+    Console.WriteLine("HardwareName:        " + output.HardwareName.GetHumanReadable());
+    Console.WriteLine("HardwareModel:       " + output.HardwareModel.GetHumanReadable());
+    Console.WriteLine("PlatformVendor:      " + output.PlatformVendor.GetHumanReadable());
+    Console.WriteLine("PlatformName:        " + output.PlatformName.GetHumanReadable());
+    Console.WriteLine("PlatformVersion:     " + output.PlatformVersion.GetHumanReadable());
+    Console.WriteLine("BrowserVendor:       " + output.BrowserVendor.GetHumanReadable());
+    Console.WriteLine("BrowserName:     " + output.BrowserName.GetHumanReadable());
+    Console.WriteLine("BrowserVersion:      " + output.BrowserVersion.GetHumanReadable());
+    Console.WriteLine("HardwareFamily:      " + output.HardwareFamily.GetHumanReadable());
+    Console.WriteLine("OEM:" + output.OEM.GetHumanReadable());
+    //Console.WriteLine(row);
+
+}
+
+
+
+public static class Extensions
+    {
+        public static string GetHumanReadable(this IAspectPropertyValue<string> apv)
+        {
+            return apv.HasValue ? apv.Value : $"Unknown ({apv.NoValueMessage})";
+        }
+        public static string GetHumanReadable(this IAspectPropertyValue<IReadOnlyList<string>> apv)
+        {
+            return apv.HasValue ? string.Join(", ", apv.Value) : $"Unknown ({apv.NoValueMessage})";
+        }
+        public static string GetHumanReadable(this IAspectPropertyValue<int> apv)
+        {
+            return apv.HasValue ? apv.Value.ToString() : $"Unknown ({apv.NoValueMessage})";
+        }
+    }
+
---
+++ ddtest/Program.cs
@@ -0,0 +1,336 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using OfficeOpenXml;
+using System.IO;
+using FiftyOne.DeviceDetection;
+using FiftyOne.DeviceDetection.Cloud.FlowElements;
+using FiftyOne.DeviceDetection.Hash.Engine.OnPremise.FlowElements;
+using FiftyOne.Pipeline.Core.Data;
+using FiftyOne.Pipeline.Core.Exceptions;
+using FiftyOne.Pipeline.Core.FlowElements;
+using FiftyOne.Pipeline.Engines;
+using FiftyOne.Pipeline.Engines.Data;
+using Microsoft.Extensions.Logging;
+
+namespace DeviceDetection
+{
+    class Program
+    {
+        public static void Main()
+        {
+            // DATAFILES
+            string Datafile1 = @"\\dpnas1\Production\daphne\hash\v4\2023\09\12\Enterprise-HashV41.hash";
+            string Datafile2 = @"\\xnas1\Test\james\pearl.daphne\pre-prod\daphne\hash\v4\2023\09\13\Enterprise-HashV41.hash";
+           // string Datafile2 = @"\\XNAS1\Test\james\pearl.daphne\20230904b\daphne\hash\v4\2023\09\03\Enterprise-HashV41.hash";
+            // string Datafile = @"\\dpnas1\Production\daphne\hash\v4\2023\09\04\Enterprise-HashV41.hash";
+
+            string filenameNew = "Top1500Crawlers";
+
+            // Filepath for Useragent Text file 
+            string filePath = $"C:\\Users\\jamesr\\Documents\\DatafileTesting\\{filenameNew}.txt";
+ 
+            string[] userAgents = File.ReadAllLines(filePath);
+
+            // Filepath for Outputted Text file
+            string OutputDest = $@"C:\Users\jamesr\Documents\DatafileTesting\Output";
+           // string OutputDest2 = $@"C:\Users\jamesr\Documents\DatafileTesting\Detection-{filenameNew}.csv";
+
+            //ask which test to run 
+            Console.WriteLine("Which test to run:\n1. Output detection in text file for Useragents\n2. Performance Tests\n 3.Input/Output Useragent on Console:\nInput (1|2|3)");
+            string whichTest = Console.ReadLine();
+
+            // Build Pipelines for different settings
+            Dictionary<(PerformanceProfiles, string), IPipeline> pipelines = new Dictionary<(PerformanceProfiles, string), IPipeline>
+                {
+                    { (PerformanceProfiles.LowMemory, Datafile1), CreatePipeline(PerformanceProfiles.LowMemory, Datafile1) },
+                    { (PerformanceProfiles.LowMemory, Datafile2), CreatePipeline(PerformanceProfiles.LowMemory, Datafile2) },
+                    { (PerformanceProfiles.Balanced, Datafile1), CreatePipeline(PerformanceProfiles.Balanced, Datafile1) },
+                    { (PerformanceProfiles.Balanced, Datafile2), CreatePipeline(PerformanceProfiles.Balanced, Datafile2) },
+                    { (PerformanceProfiles.HighPerformance, Datafile1), CreatePipeline(PerformanceProfiles.HighPerformance, Datafile1) },
+                    { (PerformanceProfiles.HighPerformance, Datafile2), CreatePipeline(PerformanceProfiles.HighPerformance, Datafile2) },
+                    { (PerformanceProfiles.MaxPerformance, Datafile1), CreatePipeline(PerformanceProfiles.MaxPerformance, Datafile1) },
+                    { (PerformanceProfiles.MaxPerformance, Datafile2), CreatePipeline(PerformanceProfiles.MaxPerformance, Datafile2) },
+            };
+
+            if (whichTest == "1")
+            {
+                using (ExcelPackage package = new ExcelPackage())
+                {
+                    ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("Properties");
+
+                    int row = 1; // Start from the first row
+                    int col = 1; // Start from the first column
+
+                    // Headers
+                    worksheet.Cells[row, col].Value = "Property";
+                    worksheet.Cells[row, col + 1].Value = "Datafile1 Value";
+                    worksheet.Cells[row, col + 2].Value = "Datafile2 Value";
+                    row++;
+
+                    foreach (string userAgent in userAgents)
+                    {
+                        // Process for Datafile1
+                        Dictionary<string, string> properties1 = ProcessUserAgent(pipelines[(PerformanceProfiles.Balanced, Datafile1)], userAgent);
+
+                        // Process for Datafile2
+                        Dictionary<string, string> properties2 = ProcessUserAgent(pipelines[(PerformanceProfiles.Balanced, Datafile2)], userAgent);
+
+                        foreach (var key in properties1.Keys)
+                        {
+                            worksheet.Cells[row, col].Value = key;
+                            worksheet.Cells[row, col + 1].Value = properties1[key];
+                            worksheet.Cells[row, col + 2].Value = properties2.ContainsKey(key) ? properties2[key] : "N/A";
+                            string comparison = (properties1[key] == properties2[key]) ? "Same" : "NotSame";
+                            worksheet.Cells[row, col + 3].Value = comparison;
+                            row++;
+                        }
+
+                        row++; // Add an empty row between different user agents
+
+
+                    }
+                    string OutputDest1 = $"{OutputDest}-Detection-{filenameNew}.xlsx";
+                    package.Workbook.Calculate();
+
+                    package.SaveAs(new FileInfo(OutputDest1));
+                }
+            }
+
+
+            else if (whichTest == "2")
+            {
+                using (ExcelPackage package = new ExcelPackage())
+                {
+                    ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("Performance Results");
+
+                    int row = 1; // Start from the first row
+                    int col = 1; // Start from the first column
+
+                    // Headers
+                    worksheet.Cells[row, col].Value = "Performance Profile";
+                    worksheet.Cells[row, col + 1].Value = "Datafile1 Time (s)";
+                    worksheet.Cells[row, col + 2].Value = "Datafile2 Time (s)";
+                    row++;
+                    worksheet.Column(1).Width = 18.57;
+                    worksheet.Column(1).Style.Font.Bold = true;
+                    worksheet.Column(1).Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
+                    worksheet.Column(1).Style.Fill.BackgroundColor.SetColor(System.Drawing.Color.LightGray);
+
+                   
+                    worksheet.Row(1).Style.Font.Bold = true;
+                    worksheet.Row(1).Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
+                    worksheet.Row(1).Style.Fill.BackgroundColor.SetColor(System.Drawing.Color.LightGray);
+
+
+
+
+                    double totalPercentageChange = 0;
+                    int totalMeasurements = 0;
+
+                    foreach (PerformanceProfiles profile in Enum.GetValues(typeof(PerformanceProfiles)))
+                    {
+
+                        if (profile == PerformanceProfiles.BalancedTemp)
+                        {
+                            continue;
+                        }
+                        worksheet.Cells[row, col].Value = $"{profile}";
+                        row++;
+
+                        
+
+
+                        for (int i = 0; i < 30; i++)
+                        {
+                            // Measure performance for Datafile1
+                            Stopwatch stopwatch1 = Stopwatch.StartNew();
+                            foreach (string userAgent in userAgents)
+                            {
+                                ProcessUserAgent(pipelines[(profile, Datafile1)], userAgent);
+                            }
+                            stopwatch1.Stop();
+
+                            // Measure performance for Datafile2
+                            Stopwatch stopwatch2 = Stopwatch.StartNew();
+                            foreach (string userAgent in userAgents)
+                            {
+                                ProcessUserAgent(pipelines[(profile, Datafile2)], userAgent);
+                            }
+                            stopwatch2.Stop();
+                            double oldValue = stopwatch1.Elapsed.TotalSeconds;
+                            double newValue = stopwatch2.Elapsed.TotalSeconds;
+                            double percentageChange = ((newValue - oldValue) / oldValue) * 100;
+                            worksheet.Cells[row, col + 1].Value = $"{stopwatch1.Elapsed.TotalSeconds:F2}";
+                            worksheet.Cells[row, col + 2].Value = $"{stopwatch2.Elapsed.TotalSeconds:F2}";
+                            worksheet.Cells[row, col + 3].Value = $"{percentageChange:F2}";
+                            totalPercentageChange += percentageChange;
+                            totalMeasurements++;
+                            row++;
+                        }
+
+                       // Add an empty row between different profiles
+                    }
+                    double overallAverage = totalPercentageChange / totalMeasurements;
+                    worksheet.Cells[row + 1, col + 3].Value = $"Overall Average: {overallAverage:F2}%";
+
+                    // Save the Excel file
+                    string OutputDest2 = $"{OutputDest}-Performance.xlsx";
+                    package.SaveAs(new FileInfo(OutputDest2));
+                }
+            }
+
+            else if (whichTest == "3")
+            {
+                Console.WriteLine("Input UserAgent(s)");
+                string input = Console.ReadLine();
+                List<string> inputUserAgents = input.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+                // Create FlowData and Get Properties 
+                foreach (string userAgent in inputUserAgents)
+                {
+                    Dictionary<string, string> properties = ProcessUserAgent(pipelines[(PerformanceProfiles.Balanced, Datafile1)], userAgent);
+                    Console.WriteLine("\nResults:\n");
+                    // Print properties to the console
+                    foreach (KeyValuePair<string, string> property in properties)
+                    {
+                        Console.WriteLine($"{property.Key}: {property.Value}");
+                    }
+
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("Invalid input.");
+            }
+        }
+
+        private static Dictionary<string, string> ProcessUserAgent(IPipeline pipeline, string userAgent)
+        {
+            using (var data = CreateFlowData(pipeline, userAgent))
+            {
+                data.Process();
+                var output = data.Get<IDeviceData>();
+
+                // Extract properties
+                Dictionary<string, string> properties = ExtractProperties(userAgent, output);
+
+                return properties;
+            }
+        }
+
+        private static IPipeline CreatePipeline(PerformanceProfiles performanceProfile, string dataFile)
+        {
+            var loggerFactory = new LoggerFactory();
+            var engine = new DeviceDetectionHashEngineBuilder(loggerFactory)
+               // .SetDrift(100) // Should look for sub strings over a wider range.
+                .SetPerformanceProfile(performanceProfile)
+                .SetUsePredictiveGraph(true)
+                .SetUsePerformanceGraph(false)
+                .SetAutoUpdate(false)
+                .SetDataFileSystemWatcher(false)
+                .Build(dataFile, false);
+            return new PipelineBuilder(loggerFactory).AddFlowElement(engine).Build();
+            //return new DeviceDetectionPipelineBuilder()
+            //    .UseOnPremise(dataFile, false)
+            //    .SetPerformanceProfile(performanceProfile)
+            //   .SetUsePredictiveGraph(true)
+            //   .SetUsePerformanceGraph(false)
+            //    .SetShareUsage(false)
+            //    .SetAutoUpdate(false)
+            //    .SetDataUpdateOnStartUp(false)
+            //    .SetDataFileSystemWatcher(false)
+            //    .Build();
+        }
+
+        private static IFlowData CreateFlowData(IPipeline pipeline, string userAgent)
+        {
+            var data = pipeline.CreateFlowData();
+            data.AddEvidence("header.user-agent", userAgent);
+            return data;
+        }
+
+        private static Dictionary<string, string> ExtractProperties(string userAgent, IDeviceData output)
+        {
+            return new Dictionary<string, string>
+    {
+        { "UserAgent", userAgent },
+        { "HardwareVendor", output.HardwareVendor.GetHumanReadable() },
+        { "HardwareName", output.HardwareName.GetHumanReadable() },
+        { "HardwareModel", output.HardwareModel.GetHumanReadable() },
+        { "PlatformVendor", output.PlatformVendor.GetHumanReadable() },
+        { "PlatformName", output.PlatformName.GetHumanReadable() },
+        { "PlatformVersion", output.PlatformVersion.GetHumanReadable() },
+        { "BrowserVendor", output.BrowserVendor.GetHumanReadable() },
+        { "BrowserName", output.BrowserName.GetHumanReadable() },
+        { "BrowserVersion", output.BrowserVersion.GetHumanReadable() },
+        { "IsCrawler", output.IsCrawler.GetHumanReadable() },
+        { "CrawlerName", output.CrawlerName.GetHumanReadable() },
+        { "UserAgents", output.UserAgents.GetHumanReadable() },
+        // Uncomment these if needed
+        // { "HardwareFamily", output.HardwareFamily.GetHumanReadable() },
+        // { "OEM", output.OEM.GetHumanReadable() }
+    };
+        }
+
+
+        private static void WritePropertiesToFile(string property, string filePath)
+        {
+            using (var textWriter = new StreamWriter(filePath, true))
+            {
+                textWriter.WriteLine(property);
+            }
+        }
+
+
+        private static void WritePropertiesToCsv(List<string> properties, string filePath)
+        {
+            using (var textWriter = new StreamWriter(filePath, true))
+            {
+                foreach (var property in properties)
+                {
+                    textWriter.WriteLine(FormatForCsv(property));
+                }
+            }
+        }
+
+        private static string FormatForCsv(string value)
+        {
+            if (value.Contains(",") || value.Contains("\"") || value.Contains("\n"))
+            {
+                return "\"" + value.Replace("\"", "\"\"") + "\"";
+            }
+            return value;
+        }
+    }
+}
+
+public static class Extensions
+{
+    public static string GetHumanReadable(this IAspectPropertyValue<string> apv)
+    {
+        return apv.HasValue ? apv.Value : $"Unknown ({apv.NoValueMessage})";
+    }
+
+
+
+    public static string GetHumanReadable(this IAspectPropertyValue<bool> apv)
+    {
+        return apv.HasValue ? apv.Value.ToString() : $"Unknown ({apv.NoValueMessage})";
+    }
+
+
+
+    public static string GetHumanReadable(this IAspectPropertyValue<IReadOnlyList<string>> apv)
+    {
+        return apv.HasValue ? string.Join("| ", apv.Value) : $"Unknown ({apv.NoValueMessage})";
+    }
+
+
+
+    public static string GetHumanReadable(this IAspectPropertyValue<int> apv)
+    {
+        return apv.HasValue ? apv.Value.ToString() : $"Unknown ({apv.NoValueMessage})";
+    }
+}
